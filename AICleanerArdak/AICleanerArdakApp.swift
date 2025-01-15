7511ee2649c7e58bc4abfb1fd8eefaad9c5a18a6
ArdakIOS <ardaktursunbayev4@gmail.com>
2025-01-15 15:45:26 +0500

    Initial Commit

---
+++ AICleanerArdak/AICleanerArdakApp.swift
@@ -0,0 +1,17 @@
+//
+//  AICleanerArdakApp.swift
+//  AICleanerArdak
+//
+//  Created by Ardak Tursunbayev on 15.01.2025.
+//
+
+import SwiftUI
+
+@main
+struct AICleanerArdakApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
